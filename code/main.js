dca2107fe6c19c3f98bd8906e63dd05a88a38022
singerbladerunnerrat <81724699+singerbladerunnerrat@users.noreply.github.com>
2021-04-01 07:12:59 -0500

    Update main.js

--- code/main.js
+++ code/main.js
@@ -1,7 +1,14 @@
 function main()
 {
-	var element = document.createElement("ul");
+	var element = document.createElement("table");
 	document.body.appendChild(element);
+	
+	for (var count = 0; count < 255; count++)
+	{
+		var string = count.toString(16);
+		console.log(string);
+	}
+	
 	console.log(window.document.body);
 }
 
