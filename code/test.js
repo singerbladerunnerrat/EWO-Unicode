8e85015c0ef272931c0fe244d1511f6fe618bad7
singerbladerunnerrat <81724699+singerbladerunnerrat@users.noreply.github.com>
2021-04-01 18:32:19 -0500

    Create test.js

---
+++ code/test.js
@@ -0,0 +1,16 @@
+		if (!(count1 % 16))
+		{
+			var tr = document.createElement("tr");
+			tbody.appendChild(tr);
+		
+			var td = document.createElement("td");
+			tr.appendChild(td);
+			
+			for (var count3 = 0; count3 < 16; count3++)
+			{
+				td = document.createElement("td");
+				td.innerHTML = "?" + count3.toString(16);
+				tr.appendChild(td);
+			}
+		}
+		
