2a604be9fcc8fb02801f1c6adc2df9ff47b06b64
singerbladerunnerrat <81724699+singerbladerunnerrat@users.noreply.github.com>
2021-04-01 18:38:39 -0500

    Update main.js

--- code/main.js
+++ code/main.js
@@ -10,7 +10,7 @@ function main()
 	var tbody = document.createElement("tbody");
 	table.appendChild(tbody);
 		
-	for (var count1 = 32; count1 < 64; count1++)
+	for (var count1 = 2; count1 < 128; count1++)
 	{
 		var tr = document.createElement("tr");
 		tbody.appendChild(tr);
