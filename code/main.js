e4e47dd442529f383c56660aeed7fdb796536e73
singerbladerunnerrat <81724699+singerbladerunnerrat@users.noreply.github.com>
2021-04-01 13:47:04 -0500

    Update main.js

--- code/main.js
+++ code/main.js
@@ -3,7 +3,7 @@ function main()
 	var table = document.createElement("table");
 	document.body.appendChild(table);
 	
-	for (var count1 = parseInt("1f50", 16); count1 < parseInt("1f53", 16); count1++)
+	for (var count1 = parseInt("1f50", 16); count1 < parseInt("1f54", 16); count1++)
 	{
 		var tr = document.createElement("tr");
 		document.body.appendChild(tr);
