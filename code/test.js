e6401e0770db6e1547dcf0a09d2ad57d153ad4ee
singerbladerunnerrat <81724699+singerbladerunnerrat@users.noreply.github.com>
2021-04-01 21:30:30 -0500

    Update test.js

--- code/test.js
+++ code/test.js
@@ -1,18 +1,1 @@
-parseInt("2", 16)  // Convert a hexadecimal number to a decimal number.
-count1.toString(16)  // Convert a decimal number to a hexadecimal number.
 
-		if (!(count1 % 16))
-		{
-			var tr = document.createElement("tr");
-			tbody.appendChild(tr);
-		
-			var td = document.createElement("td");
-			tr.appendChild(td);
-			
-			for (var count3 = 0; count3 < 16; count3++)
-			{
-				td = document.createElement("td");
-				td.innerHTML = "?" + count3.toString(16);
-				tr.appendChild(td);
-			}
-		}
